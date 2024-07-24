76f8b338ad6a9ff5b7265fe08fce21bdc36c5b03
Jennifer Chen <shinyi.chen@gmail.com>
2024-07-24 14:10:35 -0700

    Show ADS or Scix based on env var

--- components/Layout/Meta/Meta.tsx
+++ components/Layout/Meta/Meta.tsx
@@ -1,28 +1,33 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import useVariant from "../../../hooks/useVariant";
 
 /**
  * Renders metadata.
  */
 const Meta = () => {
-    return (
-        <Head>
-            <title>ADS Scan Explorer</title>
-            <link
-                rel="apple-touch-icon"
-                sizes="180x180"
-                href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/apple-touch-icon.png`}
-            />
-            <link
-                rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/favicon-16x16.png`}
-            />
+  const variant = useVariant();
+  return (
+    <Head>
+      <title>{`${variant} Scan Explorer`}</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/${
+          variant === "ADS" ? "ads" : "scix"
+        }-apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon/${
+          variant === "ADS" ? "ads" : "scix"
+        }-favicon-16x16.png`}
+      />
 
-            <meta name="description" content={`ADS Scan Explorer`} />
-        </Head>
-    )
-}
+      <meta name="description" content={`${variant} Scan Explorer`} />
+    </Head>
+  );
+};
 
-export default Meta
+export default Meta;
--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,48 +1,64 @@
-import { FC } from 'react'
-import styles from './Navbar.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRotateBack } from '@fortawesome/free-solid-svg-icons'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { FC } from "react";
+import styles from "./Navbar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateBack } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import Link from "next/link";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import useVariant from "../../../hooks/useVariant";
 
 interface AdsNavbarProps {
-    adsUrl : string;
+  adsUrl: string;
 }
 
 /**
  * Render the topmost navigation bar.
  */
-const AdsNavbar: FC<AdsNavbarProps> = ({adsUrl = process.env.NEXT_PUBLIC_ADS_DEFAULT_URL}) => {
-    return (
-        <>
-            <Navbar className={styles.navbarContainer} bg="dark" variant="dark" expand="lg">
-                <Container fluid>
-                    <Link href="/" passHref>
-                        <Navbar.Brand>
-                            <div id={styles.brandContainer} >
-                                <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH}/assets/ads.svg`} alt={"ADS logotype"} width={50} height={50} />
-                                <h1><b>Scan Explorer</b></h1>
-                            </div>
-                        </Navbar.Brand>
-                    </Link>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll" className="justify-content-end ">
-                        <Nav>
-                            <Link href={adsUrl} passHref>
-                                <Nav.Link>
-                                    <FontAwesomeIcon icon={faRotateBack} />
-                                    {' '}
-                                    Return to ADS
-                                </Nav.Link>
-                            </Link>
+const AdsNavbar: FC<AdsNavbarProps> = ({
+  adsUrl = process.env.NEXT_PUBLIC_ADS_DEFAULT_URL,
+}) => {
+  const variant = useVariant();
+  return (
+    <>
+      <Navbar
+        className={styles.navbarContainer}
+        bg="dark"
+        variant="dark"
+        expand="lg"
+      >
+        <Container fluid>
+          <Link href="/" passHref>
+            <Navbar.Brand>
+              <div id={styles.brandContainer}>
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_BASE_PATH}/assets/${
+                    variant === "ADS" ? "ads" : "scix"
+                  }.svg`}
+                  alt={`${variant === "ADS" ? "ADS" : "SciX"} logotype`}
+                  width={30}
+                  height={30}
+                />
+                <h1>
+                  <b>Scan Explorer</b>
+                </h1>
+              </div>
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll" className="justify-content-end ">
+            <Nav>
+              <Link href={adsUrl} passHref>
+                <Nav.Link>
+                  <FontAwesomeIcon icon={faRotateBack} />
+                  {` Return to ${variant === "ADS" ? "ADS" : "SciX"}`}
+                </Nav.Link>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
-
-export default AdsNavbar
+export default AdsNavbar;

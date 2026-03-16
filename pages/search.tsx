16975bd63c7418eaa39f92b5c19a68fdf9376644
femalves <fernandamalves@ufrn.edu.br>
2026-03-16 17:37:39 -0400

    Fix NaN array length crash when page/limit missing from URL

--- pages/search.tsx
+++ pages/search.tsx
@@ -122,7 +122,9 @@ interface TabProps {
 
 const SearchResultTab = ({ onSearchComplete }: TabProps) => {
     const router = useRouter()
-    const { q, page = '1', limit = '10', t: tab = 'article', sort = 'bibcode', order = 'asc' } = router.query
+    const { q, page: rawPage, limit: rawLimit, t: tab = 'article', sort = 'bibcode', order = 'asc' } = router.query
+    const page = String(rawPage || '1')
+    const limit = String(rawLimit || '10')
 
     const searchUrl = `${publicRuntimeConfig.metadataServiceUrl}/${tab}/search`
     const searchQueries = { q: q, page: page, limit: limit, sort: `${sort}_${order}` }
